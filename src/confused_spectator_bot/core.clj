9e167716e1d6f985d35b6d7aa5dd3663142c1cc2
kamil.skubis <kskubis@gmail.com>
2023-11-27 14:24:05 +0100

    download exchange info from binance

--- src/confused_spectator_bot/core.clj
+++ src/confused_spectator_bot/core.clj
@@ -1,7 +1,12 @@
 (ns confused-spectator-bot.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+(defn get-data-from-binance-USDT
+  "getting data from binance"
+  []
+  (let [binance-core-addr "https://api.binance.com"
+        binance-data-endpoint "/api/v3/exchangeInfo"
+        url (str binance-core-addr binance-data-endpoint)]
+    (json/read-str (slurp url))))
